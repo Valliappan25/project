815f564b52583322020ae43bb4a0ac4a7cd7c4ed
Ramanathan <n1502752@CVDE16C-7HHKUNC.lm.lmig.com>
2021-07-26 09:08:05 +0530

    first commit

---
+++ Docker_workspace/Square.java
@@ -0,0 +1,14 @@
+public class Square
+{
+
+	public static void main(String args[])
+	{
+		int number = Integer.parseInt(args[0]);
+
+		for(int i=1;i<=number;i++)
+		{
+			System.out.println("Square of "+i+" is => "+(i*i));
+		}
+
+	}
+}
